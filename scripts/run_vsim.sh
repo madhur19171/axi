jmdeharor/pulp-platform-axi

axi_mcast_xbar: Replace demux select indices with select masks

scripts/run_vsim.sh
@@ -160,14 +160,16 @@ exec_test() {
             done
             ;;
         axi_xbar)
-            for NumMst in 1 6; do
-                for NumSlv in 1 8; do
-                    for Atop in 0 1; do
-                        for Exclusive in 0 1; do
-                            for UniqueIds in 0 1; do
-                                call_vsim tb_axi_xbar -gTbNumMst=$NumMst -gTbNumSlv=$NumSlv \
-                                        -gTbEnAtop=$Atop -gTbEnExcl=$Exclusive \
-                                        -gTbUniqueIds=$UniqueIds
+            for Multicast in 0 1; do
+                for NumMst in 1 6; do
+                    for NumSlv in 1 8; do
+                        for Atop in 0 1; do
+                            for Exclusive in 0 1; do
+                                for UniqueIds in 0 1; do
+                                    call_vsim tb_axi_xbar -gTbMulticast=$Multicast -gTbNumMst=$NumMst -gTbNumSlv=$NumSlv \
+                                            -gTbEnAtop=$Atop -gTbEnExcl=$Exclusive \
+                                            -gTbUniqueIds=$UniqueIds
+                                done
                             done
                         done
                     done

test/tb_axi_xbar.sv
@@ -26,6 +26,7 @@ module tb_axi_xbar #(
   parameter bit TbEnAtop = 1'b1,            // enable atomic operations (ATOPs)
   parameter bit TbEnExcl = 1'b0,            // enable exclusive accesses
   parameter bit TbUniqueIds = 1'b0,         // restrict to only unique IDs
+  parameter bit TbMulticast = 1'b0,         // test the multicast XBAR
   parameter int unsigned TbNumMst = 32'd6,  // how many AXI masters there are
   parameter int unsigned TbNumSlv = 32'd8   // how many AXI slaves there are
 );
@@ -258,20 +259,36 @@ module tb_axi_xbar #(
   //-----------------------------------
   // DUT
   //-----------------------------------
-  axi_xbar_intf #(
-    .AXI_USER_WIDTH ( AxiUserWidth  ),
-    .Cfg            ( xbar_cfg      ),
-    .rule_t         ( rule_t        )
-  ) i_xbar_dut (
-    .clk_i                  ( clk     ),
-    .rst_ni                 ( rst_n   ),
-    .test_i                 ( 1'b0    ),
-    .slv_ports              ( master  ),
-    .mst_ports              ( slave   ),
-    .addr_map_i             ( AddrMap ),
-    .en_default_mst_port_i  ( '0      ),
-    .default_mst_port_i     ( '0      )
-  );
+
+  if (TbMulticast) begin : g_multicast_xbar
+    axi_mcast_xbar_intf #(
+      .AXI_USER_WIDTH ( AxiUserWidth  ),
+      .Cfg            ( xbar_cfg      ),
+      .rule_t         ( rule_t        )
+    ) i_xbar_dut (
+      .clk_i                  ( clk     ),
+      .rst_ni                 ( rst_n   ),
+      .test_i                 ( 1'b0    ),
+      .slv_ports              ( master  ),
+      .mst_ports              ( slave   ),
+      .addr_map_i             ( AddrMap )
+    );
+  end else begin : g_standard_xbar
+    axi_xbar_intf #(
+      .AXI_USER_WIDTH ( AxiUserWidth  ),
+      .Cfg            ( xbar_cfg      ),
+      .rule_t         ( rule_t        )
+    ) i_xbar_dut (
+      .clk_i                  ( clk     ),
+      .rst_ni                 ( rst_n   ),
+      .test_i                 ( 1'b0    ),
+      .slv_ports              ( master  ),
+      .mst_ports              ( slave   ),
+      .addr_map_i             ( AddrMap ),
+      .en_default_mst_port_i  ( '0      ),
+      .default_mst_port_i     ( '0      )
+    );
+  end
 
   // logger for master modules
   for (genvar i = 0; i < TbNumMst; i++) begin : gen_master_logger

madhur19171/axi

scripts/run_vsim.sh
@@ -167,14 +167,16 @@ exec_test() {
             done
             ;;
         axi_xbar)
-            for NumMst in 1 6; do
-                for NumSlv in 1 8; do
-                    for Atop in 0 1; do
-                        for Exclusive in 0 1; do
-                            for UniqueIds in 0 1; do
-                                call_vsim tb_axi_xbar -gTbNumMasters=$NumMst -gTbNumSlaves=$NumSlv \
-                                        -gTbEnAtop=$Atop -gTbEnExcl=$Exclusive \
-                                        -gTbUniqueIds=$UniqueIds
+            for Multicast in 0 1; do
+                for NumMst in 1 6; do
+                    for NumSlv in 1 8; do
+                        for Atop in 0 1; do
+                            for Exclusive in 0 1; do
+                                for UniqueIds in 0 1; do
+                                    call_vsim tb_axi_xbar -gTbMulticast=$Multicast -gTbNumMasters=$NumMst -gTbNumSlaves=$NumSlv \
+                                            -gTbEnAtop=$Atop -gTbEnExcl=$Exclusive \
+                                            -gTbUniqueIds=$UniqueIds
+                                done
                             done
                         done
                     done

test/tb_axi_xbar.sv
@@ -48,8 +48,9 @@ module tb_axi_xbar #(
   /// Enable exclusive accesses
   parameter bit TbEnExcl                     = 1'b0,   
   /// Restrict to only unique IDs         
-  parameter bit TbUniqueIds                  = 1'b0       
-
+  parameter bit TbUniqueIds                  = 1'b0,      
+  /// Test the XBAR with multicast support
+  parameter bit TbMulticast                  = 1'b0
 );
 
   // TB timing parameters
@@ -279,20 +280,35 @@ module tb_axi_xbar #(
   //-----------------------------------
   // DUT
   //-----------------------------------
-  axi_xbar_intf #(
-    .AXI_USER_WIDTH ( TbAxiUserWidth  ),
-    .Cfg            ( xbar_cfg        ),
-    .rule_t         ( rule_t          )
-  ) i_xbar_dut (
-    .clk_i                  ( clk     ),
-    .rst_ni                 ( rst_n   ),
-    .test_i                 ( 1'b0    ),
-    .slv_ports              ( master  ),
-    .mst_ports              ( slave   ),
-    .addr_map_i             ( AddrMap ),
-    .en_default_mst_port_i  ( '0      ),
-    .default_mst_port_i     ( '0      )
-  );
+  if (TbMulticast) begin : g_multicast_xbar
+    axi_mcast_xbar_intf #(
+      .AXI_USER_WIDTH ( TbAxiUserWidth ),
+      .Cfg            ( xbar_cfg       ),
+      .rule_t         ( rule_t         )
+    ) i_xbar_dut (
+      .clk_i                  ( clk     ),
+      .rst_ni                 ( rst_n   ),
+      .test_i                 ( 1'b0    ),
+      .slv_ports              ( master  ),
+      .mst_ports              ( slave   ),
+      .addr_map_i             ( AddrMap )
+    );
+  end else begin : g_standard_xbar
+    axi_xbar_intf #(
+      .AXI_USER_WIDTH ( TbAxiUserWidth  ),
+      .Cfg            ( xbar_cfg        ),
+      .rule_t         ( rule_t          )
+    ) i_xbar_dut (
+      .clk_i                  ( clk     ),
+      .rst_ni                 ( rst_n   ),
+      .test_i                 ( 1'b0    ),
+      .slv_ports              ( master  ),
+      .mst_ports              ( slave   ),
+      .addr_map_i             ( AddrMap ),
+      .en_default_mst_port_i  ( '0      ),
+      .default_mst_port_i     ( '0      )
+    );
+  end
 
   // logger for master modules
   for (genvar i = 0; i < TbNumMasters; i++) begin : gen_master_logger